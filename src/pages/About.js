74caebea3844deee7d6870527421b56a071032c7
Adnan Zaib <adnanzaib6@gmail.com>
2022-01-23 22:33:10 +0500

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue nisi
+        eros, ac sollicitudin justo congue a. Maecenas massa mi, consequat id
+        mollis ut, malesuada eget lorem. Cras a aliquam lectus. Cras sed
+        fringilla ante. Proin dolor turpis, sollicitudin et semper ut, finibus
+        at erat. Cras tempor tempor fringilla. Nulla facilisi. Phasellus posuere
+        mi ante, vel tincidunt neque ornare quis. Integer scelerisque aliquam
+        lorem sed tempus. In mi diam, facilisis at sapien id, malesuada congue
+        tortor. Praesent ut leo quis lacus dapibus dapibus sit amet sit amet mi.
+        Etiam elit velit, sollicitudin et facilisis sit amet, accumsan in
+        tellus. In dictum turpis convallis malesuada congue. Mauris congue nisi
+        ipsum, eget viverra justo posuere quis. Vivamus nec pulvinar lacus.
+        Vivamus tempus felis sapien, non eleifend ante commodo ut. Donec varius
+        libero at scelerisque venenatis. Maecenas feugiat dolor mattis tortor
+        ornare, a tempus lectus sodales.
+      </p>
+    </section>
+  );
+};
+
+export default About;
